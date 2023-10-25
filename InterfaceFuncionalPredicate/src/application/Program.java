449754ba7737bfce1df4ad72d3de74ffc9b33aaf
Zake77 <izakesz@gmail.com>
2023-10-25 20:21:48 -0300

    Expressão lambda declarada

--- InterfaceFuncionalPredicate/src/application/Program.java
+++ InterfaceFuncionalPredicate/src/application/Program.java
@@ -5,6 +5,7 @@ import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
@@ -20,7 +21,12 @@ public class Program {
        // list.removeIf(p -> p.getPrice() >= 100.0);
        // list.removeIf(new ProductPredicate());
      //   list.removeIf(Product::staticProductPredicate); // REFERENCIA PARA METODO UTILIZANDO METODO ESTATICO
-        list.removeIf(Product::nonStaticProductPredicate);
+       // list.removeIf(Product::nonStaticProductPredicate);
+
+        double min = 100.0;
+        Predicate<Product> pred = p -> p.getPrice() >= min; // EXPRESSAO LAMBDA DECLARADA
+        list.removeIf(pred);
+
         for (Product p : list) {
             System.out.println(p);
         }
